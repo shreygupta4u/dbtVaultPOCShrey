bf301a9184e00dfd78bd3d780162746f8b76acaa
shrey.gupta@tredence.com <shrey.gupta@tredence.com>
2022-01-23 05:27:44 +0000

    removed additional ref

--- snapshots/sat_order_customer_details.sql
+++ snapshots/sat_order_customer_details.sql
@@ -1,8 +1,6 @@
 {% snapshot cust_snapshot %}
     {{
         config(
-        target_database='DBTVAULTORIGINAL',
-        target_schema='DEMO_DEV',
         unique_key='CUSTOMER_PK',
         strategy='check',
         check_cols = 'all'
