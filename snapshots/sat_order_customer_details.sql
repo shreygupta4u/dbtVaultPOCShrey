1abcb50ed23f22e0d64efc79327b535e89f1153a
shrey.gupta@tredence.com <shrey.gupta@tredence.com>
2022-01-23 05:28:43 +0000

    conewc

--- snapshots/sat_order_customer_details.sql
+++ snapshots/sat_order_customer_details.sql
@@ -1,6 +1,8 @@
 {% snapshot cust_snapshot %}
     {{
         config(
+        target_database='DBTVAULTORIGINAL',
+        target_schema={{env_var('DBT_TGT_SCHEMA') }},
         unique_key='CUSTOMER_PK',
         strategy='check',
         check_cols = 'all'
