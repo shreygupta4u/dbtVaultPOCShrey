cceb65ce1fcc5e02af7753216a1f8aec074cf166
shrey.gupta@tredence.com <shrey.gupta@tredence.com>
2022-01-30 14:34:56 +0000

    removed cast as varbinary

--- macros/batchid.sql
+++ macros/batchid.sql
@@ -1,3 +1,4 @@
 {% macro batchid() %}
-    , Cast ('{{ invocation_id }}' as Varbinary(100)) as batchid 
+    -- , Cast ('{{ invocation_id }}' as Varbinary(100)) as batchid 
+    , '{{ invocation_id }}' as batchid 
 {% endmacro %}
