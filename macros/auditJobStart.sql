db4859b380e38af41df57a9bf2e0051ea8b66b26
shrey.gupta@tredence.com <shrey.gupta@tredence.com>
2022-01-31 23:51:55 +0000

    cehck

--- macros/auditJobStart.sql
+++ macros/auditJobStart.sql
@@ -1,6 +1,5 @@
 {% macro auditJobStart() %}
 Insert into demo_dev.abc_job 
 (Job_ID,Batch_id,JobMaster_ID,Start_Timestamp,Job_Status)
-Select {{ invocation_id }}, 'b4650eed-6a03-4524-b99c-e30d37c70bfa', 1,CURRENT_TIMESTAMP(),'Started'
-
+Select '{{ invocation_id }}', 'b4650eed-6a03-4524-b99c-e30d37c70bfa', 1,CURRENT_TIMESTAMP(),'Started'
 {% endmacro %}
