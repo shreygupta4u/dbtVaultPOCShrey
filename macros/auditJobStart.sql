e6504a15c30e678dd0dd46c925f51e71314061dd
shrey.gupta@tredence.com <shrey.gupta@tredence.com>
2022-01-31 23:45:45 +0000

    added more columns

--- macros/auditJobStart.sql
+++ macros/auditJobStart.sql
@@ -1,3 +1,7 @@
 {% macro auditJobStart() %}
-Insert into demo_dev.job (JobStartDateTime ,modelname, BatchId) Select SYSTIMESTAMP(),'{{ this }}','{{ batchid() }}'
+ Select SYSTIMESTAMP(),'{{ this }}','{{ batchid() }}'
+Insert into demo_dev.abc_job 
+(Job_ID,Batch_id,JobMaster_ID,Start_Timestamp,Job_Status)
+Select {{ invocation_id }}, 'b4650eed-6a03-4524-b99c-e30d37c70bfa', 1,CURRENT_TIMESTAMP(),'Started'
+
 {% endmacro %}
