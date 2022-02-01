1a4081eeef2da5da032ccc04679841416fdb24bc
shrey.gupta@tredence.com <shrey.gupta@tredence.com>
2022-02-01 00:14:12 +0000

    get

--- macros/auditJobEnd.sql
+++ macros/auditJobEnd.sql
@@ -1,3 +1,3 @@
 {% macro auditJobEnd() %}
-update demo_dev.abc_job  SET End_Timestamp=CURRENT_TIMESTAMP(),Job_Status='Complete' WHERE modelname='{{ this }}'
+update demo_dev.abc_job  SET End_Timestamp=CURRENT_TIMESTAMP(),Job_Status='Complete' WHERE JobName='{{ this }}'
 {% endmacro %}
