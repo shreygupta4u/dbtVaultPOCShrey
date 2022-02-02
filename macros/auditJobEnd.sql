a406f0b88c9d43852aeb422508f21f5351b790f2
shrey.gupta@tredence.com <shrey.gupta@tredence.com>
2022-02-02 21:46:54 +0000

    added

--- macros/auditJobEnd.sql
+++ macros/auditJobEnd.sql
@@ -1,3 +1,3 @@
 {% macro auditJobEnd() %}
-update demo_dev.abc_job  SET End_Timestamp=CURRENT_TIMESTAMP(),Job_Status='Complete' WHERE JobName='{{ this }}'
+update demo_dev.abc_job  SET JobEndDateTime=CURRENT_TIMESTAMP(),Job_Status='Complete' WHERE JobName='{{ this }}'
 {% endmacro %}
--- macros/auditJobStart.sql
+++ macros/auditJobStart.sql
@@ -1,5 +1,5 @@
 {% macro auditJobStart() %}
 Insert into demo_dev.abc_job 
-(Job_ID,Batch_id,JobName,Start_Timestamp,Job_Status)
+(Job_ID,Batch_id,JobName,JobStartDateTime,Job_Status)
 Select '{{ invocation_id }}', 'b4650eed-6a03-4524-b99c-e30d37c70bfa', '{{ this }}',CURRENT_TIMESTAMP(),'Started'
 {% endmacro %}
