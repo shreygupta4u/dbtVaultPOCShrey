f209ce3901162d8e11823f06e73fa52f93c58824
Shrey Gupta <guptshrey@gmail.com>
2022-02-07 19:39:49 +0000

    removed SQL Variaables

--- macros/SQLVariable_PreHook.sql
+++
@@ -1,8 +0,0 @@
-{% macro Produce_JobId() %}
-    {%- call statement('model_id_query', fetch_result=True) -%}
-            SET model_id = (Select md5(concat(to_varchar('{{var('job_id')}}'),'-','{{this}}')))
-    {%- endcall -%}
-    {% if execute %}
-        {%- set  model_id = load_result('model_id_query') ['data'][0][0]  -%}
-    {% endif %}
-{% endmacro %}
--- models/raw_vault/Access_SQLVariable.sql
+++
@@ -1,7 +0,0 @@
-{{ config(
-    pre_hook=[
-      "{{SQLVariable_PreHook}}"
-    ]
-) }}
-
-Select $model_id as ModelName
